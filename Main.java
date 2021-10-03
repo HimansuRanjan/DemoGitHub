1759e0b8aae8032c2b13b8abc9cc1de65b88517b
Himansu ranjan patra <mzzhimansu@gmail.com>
2021-10-03 20:43:49 +0530

    minus prob

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
+        int a = 10;
+        int b = 28;
+        System.out.println("A+B = "+ (a-b));
     }
 }
