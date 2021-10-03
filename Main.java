d89f5bd376a0ac8a9b79a71776a04c1263211f83
Himansu ranjan patra <mzzhimansu@gmail.com>
2021-10-03 20:52:54 +0530

    Multiply

--- Main.java
+++ Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         int a = 10;
         int b = 28;
-        System.out.println("A+B = "+ (a-b));
+        System.out.println("A+B = "+ (a*b));
     }
 }
